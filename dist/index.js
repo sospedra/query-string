d9000067cec8b4d67b92cfb67b4e5aa95a87a8b0
Rubén Sospedra <r.sospedra@mytaxi.com>
2018-04-20 11:31:50 +0200

    Add rollup and bundle files

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,48 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+  return typeof obj;
+} : function (obj) {
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+};
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var slicedToArray = function () {
+  function sliceIterator(arr, i) {
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+
+    try {
+      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
+
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"]) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+
+    return _arr;
+  }
+
+  return function (arr, i) {
+    if (Array.isArray(arr)) {
+      return arr;
+    } else if (Symbol.iterator in Object(arr)) {
+      return sliceIterator(arr, i);
+    } else {
+      throw new TypeError("Invalid attempt to destructure non-iterable instance");
+    }
+  };
+}();
 
 var strictUriEncode = require('strict-uri-encode');
 var decodeComponent = require('decode-uri-component');
@@ -133,7 +173,7 @@ function parse(input, options) {
 			var param = _step.value;
 
 			var _param$replace$split = param.replace(/\+/g, ' ').split('='),
-			    _param$replace$split2 = _slicedToArray(_param$replace$split, 2),
+			    _param$replace$split2 = slicedToArray(_param$replace$split, 2),
 			    key = _param$replace$split2[0],
 			    value = _param$replace$split2[1];
 
@@ -177,13 +217,13 @@ exports.extract = extract;
 exports.parse = parse;
 
 exports.stringify = function (obj, options) {
-	var defaults = {
+	var defaults$$1 = {
 		encode: true,
 		strict: true,
 		arrayFormat: 'none'
 	};
 
-	options = Object.assign(defaults, options);
+	options = Object.assign(defaults$$1, options);
 
 	if (options.sort === false) {
 		options.sort = function () {};
